ac74b421b8108b7b67ae48c441a6614ddf482a4d
Ky Nguyen <kynguyen@coinhako.com>
2017-10-12 17:03:04 +0800

    Initial Commit

---
+++ knCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  knCollection
+//
+//  Created by Ky Nguyen on 10/12/17.
+//  Copyright © 2017 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
